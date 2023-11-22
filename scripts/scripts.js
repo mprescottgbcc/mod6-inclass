776c842f7739e7de669dae1d944c075a9ea051b4
Meg Prescott <mprescott@ccsnh.edu>
2023-11-21 19:16:24 -0500

    Update all starting files

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,1 @@
-/*
-    JavaScript Basics tutorial
-*/
-
-let textValue = "Hello World";
-const headingElement = document.querySelector('h1');
-headingElement.textContent = textValue;
-
-const imageElement = document.querySelector('img');
-//imageElement.style.display = "none";
-
-imageElement.classList.add('gone');
+/*JavaScript Basics tutorial*/
